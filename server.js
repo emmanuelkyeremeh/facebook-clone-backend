02ad156a4d9c330386068785a533c485ad567ad4
Emmanuel Kyeremeh <josephkyeremeh53@gmail.com>
2021-07-20 09:33:31 +0000

    new security changes

--- server.js
+++ server.js
@@ -1,30 +1,27 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8080;
 
-const app = express()
-const port = process.env.PORT || 8080
+app.use(cors());
+app.use(express.json());
 
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", () => {
+  console.log("Connected succesfully!");
+});
 
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
 
+app.get("/", (req, res) => res.send("Facebook clone backend"));
 
-app.use(cors())
-app.use(express.json())
-
-
-mongoose.connect('mongodb+srv://emmanuelkyeremeh:1234567qazplm@kepler.eequm.mongodb.net/Kepler?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error',console.error.bind(console,'connection error: '))
-db.once('open',()=>{
-    console.log('Connected succesfully!')
-})
-
-
-
-const postRouter = require('./routes/Posts')
-app.use('/posts',postRouter)
-
-
-app.get('/', (req, res) => res.send('Facebook clone backend'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
